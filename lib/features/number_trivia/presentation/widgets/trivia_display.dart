2c36f7178404386c29653358fef6809696b240ec
Ade Fikri Malihuddin <ade.fm97@gmail.com>
2021-03-10 20:45:50 +0700

    course 3 start

---
+++ lib/features/number_trivia/presentation/widgets/trivia_display.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../../domain/entities/number_trivia.dart';
+
+class TriviaDisplay extends StatelessWidget {
+  final NumberTrivia numberTrivia;
+
+  const TriviaDisplay({Key key, @required this.numberTrivia}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
